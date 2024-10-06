5285523cb5d97f5492824d1fd3870f90c087f183
petruki <31597636+petruki@users.noreply.github.com>
2024-10-06 16:35:16 -0700

    Bump test deps: oak@17.0.0

--- test/deps.ts
+++ test/deps.ts
@@ -1,4 +1,4 @@
-export { Application, Context, Router } from 'jsr:@oak/oak@16.1.0';
-export type { Next } from 'jsr:@oak/oak@16.1.0';
-export { assertEquals, assertNotEquals, assertThrows } from 'jsr:@std/assert@1.0.3';
+export { Application, Context, Router } from 'jsr:@oak/oak@17.0.0';
+export type { Next } from 'jsr:@oak/oak@17.0.0';
+export { assertEquals, assertNotEquals, assertThrows } from 'jsr:@std/assert@1.0.6';
 export { type IResponse, superoak } from 'https://deno.land/x/superoak@4.8.1/mod.ts';
