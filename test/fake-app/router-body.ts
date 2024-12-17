7218dbd880f1c9d4a6e10f1963bd05b453342b1f
petruki <31597636+petruki@users.noreply.github.com>
2024-12-16 18:14:28 -0800

    Closes #6 - added array of objects validation

--- test/fake-app/router-body.ts
+++ test/fake-app/router-body.ts
@@ -4,7 +4,7 @@ import { type Context, type Next, Router } from '../deps.ts';
 const router = new Router();
 
 const { body } = ValidatorMiddleware.createMiddleware<Context, Next>();
-const { isArray } = ValidatorFn.createValidator();
+const { isArray, hasLenght } = ValidatorFn.createValidator();
 
 router.post(
   '/validate-body1',
@@ -55,6 +55,29 @@ router.post(
   body([
     { key: 'account.name' },
     { key: 'account.phone', optional: true },
+  ]),
+  ({ response, state }: Context) => {
+    response.status = 200;
+    response.body = state.request_body;
+  },
+);
+
+router.post(
+  '/validate-body6',
+  body([
+    { key: 'colors.*.name', validators: [hasLenght({ min: 3 })] }
+  ]),
+  ({ response, state }: Context) => {
+    response.status = 200;
+    response.body = state.request_body;
+  },
+);
+
+router.post(
+  '/validate-body7',
+  body([
+    { key: 'color.list.*.name', validators: [hasLenght({ min: 3 })] },
+    { key: 'color.list.*.hex', optional: true, validators: [hasLenght({ min: 6 })] }
   ]),
   ({ response, state }: Context) => {
     response.status = 200;
