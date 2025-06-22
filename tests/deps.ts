0258fea21bc4413c1b3e07710cc2622e7c93158d
petruki <31597636+petruki@users.noreply.github.com>
2025-06-22 11:41:14 -0700

    tests: removed superoak workaround, bump to v5

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,8 +1,4 @@
 export { Application, Context, Router } from 'jsr:@oak/oak@17.1.4';
 export type { Next } from 'jsr:@oak/oak@17.1.4';
-export { assertEquals, assertNotEquals, assertThrows } from 'jsr:@std/assert@1.0.11';
-export { type IResponse, superoak } from 'https://deno.land/x/superoak@4.8.1/mod.ts';
-
-// Fixes superdeno@4.9.0 issue
-// deno-lint-ignore no-explicit-any
-(globalThis as any).window = globalThis;
+export { assertEquals, assertNotEquals, assertThrows } from 'jsr:@std/assert@1.0.13';
+export { type IResponse, superoak } from 'https://deno.land/x/superoak@5.0.0/mod.ts';
