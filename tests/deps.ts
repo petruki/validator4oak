a9010b98c571e10c4284c541c6746bbd83d90d18
petruki <31597636+petruki@users.noreply.github.com>
2025-08-30 14:15:11 -0700

    chore: bump oak test server to 17.1.6, sonar action 5.3.1

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,3 +1,3 @@
-export { Application, Context, Router, type Next } from 'jsr:@oak/oak@17.1.5';
-export { assertEquals, assertNotEquals, assertThrows } from 'jsr:@std/assert@1.0.13';
+export { Application, Context, Router, type Next } from 'jsr:@oak/oak@17.1.6';
+export { assertEquals, assertNotEquals, assertThrows } from 'jsr:@std/assert@1.0.14';
 export { type IResponse, superoak } from 'https://deno.land/x/superoak@5.0.0/mod.ts';
